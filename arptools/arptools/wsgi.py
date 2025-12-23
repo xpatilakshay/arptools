ff0388543fa4a3e0b31f15297fcbad7218c2536f
Akshay Patil <161199431+xpatilakshay@users.noreply.github.com>
2025-12-23 12:26:48 +0000

    Added code structure

--- arptools/arptools/wsgi.py
+++ arptools/arptools/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arptools.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arptools.settings")
 
 application = get_wsgi_application()
